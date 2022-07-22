de1c4487f54598b52ece9f0dbcf3fc16af33bedd
lyidle <912512766@qq.com>
2022-07-22 17:31:30 +0800

    练习

---
+++ JavaScript部分/放大镜/js/index.js
@@ -0,0 +1,126 @@
+let obj = [{
+        index: 0,
+        src: './images/1.jpg'
+    },
+    {
+        index: 1,
+        src: './images/2.jpg'
+    },
+    {
+        index: 2,
+        src: './images/3.jpg'
+    },
+    {
+        index: 3,
+        src: './images/4.jpg'
+    },
+]
+// 第一个参数，图片数据，第二个参数div类名，第三个参数指定父元素
+scale(obj, '.maxScale','body')
+function scale(Data, maxBox, parent) {
+    // 创建元素
+    let frag = document.createDocumentFragment(),
+        // samll盒子
+        smallElement = document.createElement('div')
+    smallElement.className = 'smallImg'
+    let img = document.createElement('img');
+    let span = document.createElement('span');
+    img.src = './images/1.jpg'
+    smallElement.appendChild(img)
+    // mask
+    span.className = 'mask'
+    smallElement.appendChild(span)
+    frag.appendChild(smallElement)
+    let max = document.querySelector(`${maxBox}`)
+    max.appendChild(frag)
+    // max盒子
+    let maxElement = document.createElement('div')
+    maxElement.className = 'maxImg'
+    let maxImg = img.cloneNode()
+    maxElement.appendChild(maxImg)
+    frag.appendChild(maxElement)
+    max.appendChild(frag)
+    // list
+    let list = document.createElement('ul')
+    list.className = 'list'
+    Data.forEach((val) => {
+        let li = document.createElement('li'),
+            img = document.createElement('img')
+        img.src = val.src
+        // 第一个li添加点击类名
+        if (val.index === 0) {
+            li.classList.add('active')
+        }
+        list.appendChild(li)
+        li.appendChild(img)
+        frag.appendChild(list)
+    })
+    max.appendChild(frag)
+    let li = null,
+        imges = null,
+        maxImgs = null
+    if (parent === undefined) {
+        // list里的li
+        li = document.querySelectorAll(`${maxBox}>.list>li`)
+        // 小图片
+        imges = document.querySelector(`${maxBox}>.smallImg>img`)
+        // 大图片
+        maxImgs = document.querySelector(`${maxBox}>.maxImg>img`)
+    } else {
+        // list里的li
+        li = document.querySelectorAll(`${parent}>${maxBox}>.list>li`)
+        // 小图片
+        imges = document.querySelector(`${parent}>${maxBox}>.smallImg>img`)
+        // 大图片
+        maxImgs = document.querySelector(`${parent}>${maxBox}>.maxImg>img`)
+    }
+    // 移入
+    smallElement.onmouseenter = function (e) {
+        maxElement.style.display = 'block'
+        span.style.display = 'block'
+        // 范围
+        let postionX = this.offsetWidth - span.offsetWidth,
+            postionY = this.offsetHeight - span.offsetHeight
+        smallElement.onmousemove = function (e) {
+            // 移动
+            let x = e.offsetX - span.offsetWidth * 0.5,
+                y = e.offsetY - span.offsetHeight * 0.5
+            // 左右
+            // if (x < 0) {
+            //     x = 0
+            // }
+            // if(x>postionX){
+            //     x=postionX
+            // }
+            x < 0 ? x = 0 : x > postionX ? x = postionX : null
+            // 上下
+            y < 0 ? y = 0 : y > postionY ? y = postionY : null
+            span.style.cssText += `
+        left:${x}px;
+        top:${y}px;
+        `
+            maxImgs.style.cssText += `
+        left:${-x*(maxImgs.offsetWidth/this.offsetWidth)}px;
+        top:${-y*(maxImgs.offsetWidth/this.offsetWidth)}px;
+        `
+        }
+    }
+    // 移出
+    smallElement.onmouseleave = function () {
+        maxElement.style.display = 'none'
+        span.style.display = 'none'
+    }
+
+    // 点击切换
+    li.forEach((val) => {
+        val.onclick = function () {
+            let imgs = this.children[0].src
+            imges.src = imgs
+            maxImgs.src = imgs
+            for (let i = 0; i < li.length; i++) {
+                li[i].classList.remove('active')
+            }
+            this.classList.add('active')
+        }
+    })
+}
---
+++ JavaScript部分/瀑布流/js/index.js
@@ -0,0 +1,533 @@
+// 模拟数据
+let imgs = [{
+        index: 1,
+        src: "./images/1.png"
+    },
+    {
+        index: 2,
+        src: "./images/2.jpg"
+    },
+    {
+        index: 3,
+        src: "./images/3.png"
+    },
+    {
+        index: 4,
+        src: "./images/4.jpg"
+    },
+    {
+        index: 5,
+        src: "./images/5.png"
+    },
+    {
+        index: 6,
+        src: "./images/6.jpg"
+    },
+    {
+        index: 7,
+        src: "./images/7.png"
+    },
+    {
+        index: 8,
+        src: "./images/8.jpg"
+    },
+    {
+        index: 9,
+        src: "./images/9.png"
+    },
+    {
+        index: 10,
+        src: "./images/10.jpg"
+    },
+    {
+        index: 11,
+        src: "./images/11.png"
+    },
+    {
+        index: 12,
+        src: "./images/12.jpg"
+    },
+    {
+        index: 13,
+        src: "./images/13.png"
+    },
+    {
+        index: 14,
+        src: "./images/14.jpg"
+    },
+    {
+        index: 15,
+        src: "./images/15.png"
+    },
+    {
+        index: 16,
+        src: "./images/16.jpg"
+    },
+    {
+        index: 17,
+        src: "./images/17.png"
+    },
+    {
+        index: 18,
+        src: "./images/18.jpg"
+    },
+    {
+        index: 19,
+        src: "./images/19.png"
+    },
+    {
+        index: 20,
+        src: "./images/20.jpg"
+    },
+    {
+        index: 21,
+        src: "./images/21.png"
+    },
+    {
+        index: 22,
+        src: "./images/22.jpg"
+    },
+    {
+        index: 23,
+        src: "./images/23.png"
+    },
+    {
+        index: 24,
+        src: "./images/24.jpg"
+    },
+    {
+        index: 25,
+        src: "./images/25.jpg"
+    },
+    {
+        index: 26,
+        src: "./images/26.jpg"
+    },
+    {
+        index: 27,
+        src: "./images/27.jpg"
+    },
+    {
+        index: 28,
+        src: "./images/28.png"
+    },
+    {
+        index: 29,
+        src: "./images/29.jpg"
+    },
+    {
+        index: 30,
+        src: "./images/30.png"
+    },
+    {
+        index: 31,
+        src: "./images/31.jpg"
+    },
+    {
+        index: 32,
+        src: "./images/32.png"
+    },
+    {
+        index: 33,
+        src: "./images/33.jpg"
+    },
+    {
+        index: 34,
+        src: "./images/34.png"
+    },
+    {
+        index: 35,
+        src: "./images/35.jpg"
+    },
+    {
+        index: 36,
+        src: "./images/36.png"
+    },
+    {
+        index: 37,
+        src: "./images/37.jpg"
+    },
+    {
+        index: 38,
+        src: "./images/38.png"
+    },
+    {
+        index: 39,
+        src: "./images/39.jpg"
+    },
+    {
+        index: 40,
+        src: "./images/40.png"
+    },
+    {
+        index: 41,
+        src: "./images/41.jpg"
+    },
+    {
+        index: 42,
+        src: "./images/42.png"
+    },
+    {
+        index: 43,
+        src: "./images/43.jpg"
+    },
+    {
+        index: 44,
+        src: "./images/44.png"
+    },
+    {
+        index: 45,
+        src: "./images/45.jpg"
+    },
+    {
+        index: 46,
+        src: "./images/46.png"
+    },
+    {
+        index: 47,
+        src: "./images/47.jpg"
+    },
+    {
+        index: 48,
+        src: "./images/48.png"
+    },
+    {
+        index: 49,
+        src: "./images/49.jpg"
+    },
+    {
+        index: 50,
+        src: "./images/50.png"
+    },
+    {
+        index: 51,
+        src: "./images/51.jpg"
+    },
+    {
+        index: 52,
+        src: "./images/52.png"
+    },
+    {
+        index: 53,
+        src: "./images/53.jpg"
+    },
+    {
+        index: 54,
+        src: "./images/54.png"
+    },
+    {
+        index: 55,
+        src: "./images/55.jpg"
+    },
+    {
+        index: 56,
+        src: "./images/56.png"
+    },
+    {
+        index: 57,
+        src: "./images/57.jpg"
+    },
+    {
+        index: 58,
+        src: "./images/58.png"
+    },
+    {
+        index: 59,
+        src: "./images/59.jpg"
+    },
+    {
+        index: 60,
+        src: "./images/60.png"
+    },
+    {
+        index: 61,
+        src: "./images/61.jpg"
+    },
+    {
+        index: 62,
+        src: "./images/62.png"
+    },
+    {
+        index: 63,
+        src: "./images/63.jpg"
+    },
+    {
+        index: 64,
+        src: "./images/64.png"
+    },
+    {
+        index: 65,
+        src: "./images/65.jpg"
+    },
+    {
+        index: 66,
+        src: "./images/66.png"
+    },
+    {
+        index: 67,
+        src: "./images/67.jpg"
+    },
+    {
+        index: 68,
+        src: "./images/68.png"
+    },
+    {
+        index: 69,
+        src: "./images/69.jpg"
+    },
+    {
+        index: 70,
+        src: "./images/70.png"
+    },
+    {
+        index: 71,
+        src: "./images/71.jpg"
+    },
+    {
+        index: 72,
+        src: "./images/72.png"
+    },
+    {
+        index: 73,
+        src: "./images/73.jpg"
+    },
+    {
+        index: 74,
+        src: "./images/74.png"
+    },
+    {
+        index: 75,
+        src: "./images/75.jpg"
+    },
+    {
+        index: 76,
+        src: "./images/76.png"
+    },
+    {
+        index: 77,
+        src: "./images/77.jpg"
+    },
+    {
+        index: 78,
+        src: "./images/78.png"
+    },
+    {
+        index: 79,
+        src: "./images/79.jpg"
+    },
+    {
+        index: 80,
+        src: "./images/80.png"
+    },
+    {
+        index: 81,
+        src: "./images/81.jpg"
+    },
+    {
+        index: 82,
+        src: "./images/82.png"
+    },
+    {
+        index: 83,
+        src: "./images/83.jpg"
+    },
+    {
+        index: 84,
+        src: "./images/84.png"
+    },
+    {
+        index: 85,
+        src: "./images/85.jpg"
+    },
+    {
+        index: 86,
+        src: "./images/86.png"
+    },
+    {
+        index: 87,
+        src: "./images/87.jpg"
+    },
+    {
+        index: 88,
+        src: "./images/88.png"
+    },
+    {
+        index: 89,
+        src: "./images/89.jpg"
+    },
+    {
+        index: 90,
+        src: "./images/90.png"
+    },
+    {
+        index: 91,
+        src: "./images/91.jpg"
+    },
+    {
+        index: 92,
+        src: "./images/92.png"
+    },
+    {
+        index: 93,
+        src: "./images/93.jpg"
+    },
+    {
+        index: 94,
+        src: "./images/94.png"
+    },
+    {
+        index: 95,
+        src: "./images/95.jpg"
+    },
+    {
+        index: 96,
+        src: "./images/96.png"
+    },
+    {
+        index: 97,
+        src: "./images/97.jpg"
+    },
+    {
+        index: 98,
+        src: "./images/98.png"
+    },
+    {
+        index: 99,
+        src: "./images/99.jpg"
+    },
+    {
+        index: 100,
+        src: "./images/100.png"
+    },
+    {
+        index: 101,
+        src: "./images/101.jpg"
+    },
+    {
+        index: 102,
+        src: "./images/102.png"
+    },
+    {
+        index: 103,
+        src: "./images/103.jpg"
+    },
+    {
+        index: 104,
+        src: "./images/104.png"
+    },
+    {
+        index: 105,
+        src: "./images/105.jpg"
+    },
+    {
+        index: 106,
+        src: "./images/106.png"
+    },
+    {
+        index: 107,
+        src: "./images/107.jpg"
+    },
+]
+waterfallList(imgs)
+// 数据生成
+function waterfallList(Data) {
+    let farg = document.createDocumentFragment()
+    Data.forEach(val => {
+        let div = document.createElement('div'),
+            img = document.createElement('img')
+        div.className = 'list'
+        img.src = val.src
+        div.appendChild(img)
+        farg.appendChild(div)
+    });
+    let max = document.querySelector('.max')
+    max.appendChild(farg)
+}
+window.onload = () => {
+    waterfall()
+    window.onscroll = () => {
+        if (reload()) {
+            let parent = document.querySelector('.max')
+            // 模拟数据  imgs 可以自定义数据
+            let farg = document.createDocumentFragment()
+            imgs.forEach(val => {
+                let div = document.createElement('div'),
+                    img = document.createElement('img')
+                div.className = 'list'
+                img.src = val.src
+                div.appendChild(img)
+                farg.appendChild(div)
+            });
+            parent.appendChild(farg)
+            // 重新布局
+            waterfall()
+        }
+    }
+    window.onresize = () => {
+        waterfall()
+    }
+}
+// 第一个父级
+function waterfall() {
+    // 获取父级与子集
+    let wrap = document.querySelector('.max'),
+        item = document.querySelectorAll('.max .list')
+    // 第一个盒子的宽度
+    let boxWidth = item[0].offsetWidth
+    // 可视区域宽度
+    let pagex = document.documentElement.clientWidth
+    // 列数
+    clos = ~~(pagex / boxWidth)
+    // 父盒子居中
+    wrap.style.cssText = `
+    width:${boxWidth*clos}px;
+    margin:0 auto;
+    `
+    // 子元素高度数组  子元素高度 最小子元素高度 最小子元素索引
+    let heightArray = [],
+        itemHeight = 0,
+        minItemHeight = 0,
+        minItemIndex = 0
+    for (i = 0; i < item.length; i++) {
+        itemHeight = item[i].offsetHeight
+        if (i < clos) {
+            // 第一行的高度放到数组里
+            heightArray.push(itemHeight)
+            // 清空第一行的style，防止改变页面时加上
+            item[i].style=''
+        } else {
+            // 超出第一行时
+            // 找出高度最低的元素
+            let arr = [...heightArray]
+            // 最小的高度
+            minItemHeight = arr.sort((a, b) => {
+                return a - b
+            })[0]
+            // 最小盒子的索引
+            minItemIndex = heightArray.indexOf(minItemHeight)
+            // 对应样式
+            item[i].style.position = 'absolute'
+            item[i].style.left = minItemIndex * boxWidth + 'px'
+            item[i].style.top = minItemHeight + 'px'
+            // 更新高度  最小的索引基础上增加 超出第一行后每次进来都是找最小的高度放到那个元素的下面
+            heightArray[minItemIndex] += itemHeight
+        }
+    }
+}
+
+// 滚动加载
+function reload() {
+    let item = document.querySelectorAll('.max .list')
+    // 获取滚动条的距离
+    let scrollY = document.documentElement.scrollTop
+    // 可视区域的高度
+    let clientY = document.documentElement.clientHeight
+    // 最后一个盒子距离父级顶部的距离
+    let lastItem = item[item.length - 1]
+    let lastItemY = lastItem.offsetTop
+    // 页面高度
+    let pageHeight = clientY + scrollY
+    document.title = `${lastItemY}---${pageHeight}`
+    // 超过就返回true 没超过就false
+    return lastItemY < pageHeight ? true : false
+}
